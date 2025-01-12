1810c216a8f0360bcd8d16a3fb11a5c929e486ed
DhanushVDPT <87219329+DhanushVDPT@users.noreply.github.com>
2025-01-12 10:53:21 +0530

    Update leadingothers.js

--- pages/leadingothers.js
+++ pages/leadingothers.js
@@ -6,7 +6,7 @@ export default function Page() {
     return (
         <>
             <Head>
-                <title>Rishika's Portfolio | Leading Others</title>
+                <title>Rishika&apos;s Portfolio | Leading Others</title>
             </Head>
             <div className="content">
                 <h1>Chapter 4.2: Leading Others</h1>
